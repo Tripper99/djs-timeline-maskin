d1aa6f5a8c7a84d1992e07f8f6f29854406c22c0
Dan Josefsson <dan@josefsson.net>
2026-02-18 16:51:04 +0100

    v2.7.1: Plan - Fix layout height, preview full-height, auto-move PDFs
    Three fixes from user feedback on v2.7.0:
    1. File list area should expand to fill full window height when maximized
    2. PDF preview should span full vertical height (top to bottom)
    3. Processed PDFs should auto-move to output folder even if not renamed

--- utils/constants.py
+++ utils/constants.py
@@ -4,8 +4,8 @@ Constants and configuration values for the DJ Timeline application
 
 # Configuration
 CONFIG_FILE = "djs_timeline_machine_config.json"
-VERSION = "v2.7.0"
-VERSION_TUPLE = (2, 7, 0)
+VERSION = "v2.7.1"
+VERSION_TUPLE = (2, 7, 1)
 
 # GitHub Repository Information for Updates
 GITHUB_REPO_OWNER = "Tripper99"
