4e6c992836870a40b68c73d47b8b7146e5f541a6
Dan Josefsson <dan@josefsson.net>
2026-02-22 12:37:43 +0100

    v2.8.8: Pre-commit before auto-refresh file list after 'Spara allt och rensa'

--- utils/constants.py
+++ utils/constants.py
@@ -4,8 +4,8 @@ Constants and configuration values for the DJ Timeline application
 
 # Configuration
 CONFIG_FILE = "djs_timeline_machine_config.json"
-VERSION = "v2.8.7"
-VERSION_TUPLE = (2, 8, 7)
+VERSION = "v2.8.8"
+VERSION_TUPLE = (2, 8, 8)
 
 # GitHub Repository Information for Updates
 GITHUB_REPO_OWNER = "Tripper99"
